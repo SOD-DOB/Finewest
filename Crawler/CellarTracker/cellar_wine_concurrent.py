64fc92d12f6f26ca2e83cb45482c6c3158e5583c
SOD <842496745@qq.com>
2019-05-17 11:08:55 +0800

    test

--- Crawler/CellarTracker/cellar_wine_concurrent.py
+++ Crawler/CellarTracker/cellar_wine_concurrent.py
@@ -1,5 +1,4 @@
 import requests
-import numpy as np
 import pymysql
 import re
 import time
@@ -7,8 +6,9 @@ import random
 import math
 from lxml import html as HTML
 import threading
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed, wait, FIRST_COMPLETED
 import unicodedata
+
 sqlLock = threading.Lock()
 UA = [
     "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)",
@@ -44,15 +44,15 @@ proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
     "user": 'H6B8WY3O4988764P',
     "pass": 'B44BC8FAD71691A4',
 }
-proxies = {'https':proxyMeta,
-           'http':proxyMeta, }
+proxies = {'https': proxyMeta,
+           'http': proxyMeta, }
 
 header = {
-    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language':'zh-CN,zh;q=0.9',
-    'Connection':'keep-alive',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Connection': 'keep-alive',
     'Host': 'www.cellartracker.com',
-    'Referer':'https://www.cellartracker.com/',
+    'Referer': 'https://www.cellartracker.com/',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': random.choice(UA),
 }
@@ -77,16 +77,17 @@ sql2 = pymysql.connect(
 )
 cursor2 = sql2.cursor()
 
+
 def getRules():
     # 所有的
     goodsrules = "SELECT `id` FROM `shopping_goodsrules`"
     sqlLock.acquire()
-    cursor1.execute (goodsrules)
+    cursor1.execute(goodsrules)
     sqlLock.release()
     rules_list = cursor1.fetchall()
     rulesid = [str(i[0]) for i in rules_list]
     # 已查询的
-    testrules = "SELECT `classNameId` FROM `crawl_cellar` group by `classNameId`"
+    testrules = "SELECT `classNameId` FROM `crawl` WHERE `from`='CT' and type=1 group by `classNameId`"
     sqlLock.acquire()
     cursor2.execute(testrules)
     sqlLock.release()
@@ -96,82 +97,86 @@ def getRules():
     idlist = [id for id in rulesid if id not in cellar]
     return idlist
 
-def re_name(x):
-    try:
-        a = "".join (re.compile(r'[^\u4E00-\u9FA5]').findall(x))
-        b = re.findall(r'[(](.*?)[)]', str(a))[0]
-        name = a.replace('(' + b + ')','').strip()
-    except:
-        name = "".join(re.compile (r'[^\u4E00-\u9FA5]').findall(x))
-    return name # 非中文
 
 def tryUrl(url):
     while True:
         try:
-            response = requests.get(url=url, headers=header, proxies=proxies)#verify=False,.content.decode('utf-8')
-            time.sleep(2)
-            html = HTML.fromstring(response.content)
-            return html
+            response = requests.get(url=url, headers=header, proxies=proxies)  # verify=False,.content.decode('utf-8')
+            if response.status_code == 200:
+                html = HTML.fromstring(response.content.decode('utf-8'))
+                return html
+            else:
+                time.sleep(random.uniform(1, 2))
+                continue
         except:
             # print('\r\n   解析失败，重试中')
-            time.sleep(random.uniform(3,8))
             continue
 
+
 def changUni(unic):
-    change = unicodedata.normalize ('NFKD', unic).encode('ascii', 'ignore').strip() # 转换法语
+    change = unicodedata.normalize('NFKD', unic).encode('ascii', 'ignore').strip()  # 转换法语
     code = str(change, encoding='utf-8')
     return code
 
-def getmess(url,xpathtree):
+
+def getmess(url, xpathtree):
     while True:
         html = tryUrl(url)
         mess = html.xpath(xpathtree)
-        if len(mess)>0:
-            return html,mess
+        if len(mess) > 0:
+            return html, mess
         else:
             continue
+
 
 def crawl(id):
     goodsrules = "SELECT `className` FROM `shopping_goodsrules` where id =" + str(id)
     sqlLock.acquire()
     cursor1.execute(goodsrules)
     sqlLock.release()
-    old_rule = cursor1.fetchall()[0][0].strip()
+    old_rule = cursor1.fetchall()[0][0].replace('\xa0', ' ').strip()
     print('\r\n查找:', id, old_rule)
-    newrule = changUni(re_name(old_rule)).strip()
+    time.sleep(10)
+    newrule = changUni(old_rule).replace("(", "").replace(")", "").replace("（", "").replace("）", "").strip()
     url = "https://www.cellartracker.com/list.asp?fInStock=0&iUserOverride=0&Table=List&szSearch=" + str(
-        newrule).replace(' ', '%20')
+        newrule).replace(' ', '%20').replace("'", "%92").replace("&", "%26")
     resultsxpath = "string(//div[@id='narrow_results']/a[1]/span)"
     time.sleep(random.uniform(1, 3))
-    results = getmess(url, resultsxpath)[1].replace(' Wines', '').replace(' ', '')
+    results = getmess(url, resultsxpath)[1].replace(' Wines', '').replace(',', '').replace('+', '')
     if int(results) > 0:
-        perpage = getmess(url, resultsxpath)[0].xpath('//a[@id="top_paging"]/text()')[0]
-        pages = math.ceil(int(results) / int(perpage))
-        for page in range(pages):
-            spuurl = url+'&Page='+str(page+1)
+        try:
+            pagesResult = getmess(url, resultsxpath)[0].xpath('//form[@id="top_goto"]/a/text()')[0]
+            pages = re.findall("of(.*)", pagesResult)[0].strip()
+        except IndexError:
+            perpage = getmess(url, resultsxpath)[0].xpath('//a[@id="top_paging"]/text()')[0]
+            pages = math.ceil(int(results) / int(perpage))
+        for page in range(int(pages)):
+            spuurl = url + '&Page=' + str(page + 1)
             hrefxpath = "//table[@id='main_table']//tr/td[1]//@href"
-            hrefs = getmess(spuurl,hrefxpath)[1]
+            hrefs = getmess(spuurl, hrefxpath)[1]
             for href in hrefs:
-                time.sleep (random.uniform (1, 3))
-                wineurl = "https://www.cellartracker.com/"+str(href)
+                time.sleep(random.uniform(1, 3))
+                wineurl = "https://www.cellartracker.com/" + str(href)
                 namemessxpath = "string(//div[@id='wine_copy_inner']/h1)"
-                winehtml = getmess(wineurl,namemessxpath)[0]
-                namemess = getmess(wineurl,namemessxpath)[1]
+                winehtml = getmess(wineurl, namemessxpath)[0]
+                namemess = getmess(wineurl, namemessxpath)[1]
                 try:
-                    score = winehtml.xpath('string(//div[@id="wine_copy_inner"]/div[1]/span[1])')+winehtml.xpath('string(//div[@id="wine_copy_inner"]/div[1]/span[2])')
+                    score = winehtml.xpath('string(//div[@id="wine_copy_inner"]/div[1]/span[1])') + winehtml.xpath(
+                        'string(//div[@id="wine_copy_inner"]/div[1]/span[2])')
                 except:
                     score = ''
                 try:
                     try:
-                        vintage = re.findall('\d{4}',namemess)[0]
+                        vintage = re.findall('\\d{4}', namemess)[0]
                     except IndexError:
-                        vintage = re.findall('NV',namemess)[0]
+                        vintage = re.findall('NV', namemess)[0]
                 except:
                     vintage = ''
-                name = namemess.replace(vintage,'').strip()+' '+winehtml.xpath('string(//div[@id="wine_copy_inner"]/h2)').strip()
+                name = namemess.replace(vintage, '').strip() + ' ' + winehtml.xpath(
+                    'string(//div[@id="wine_copy_inner"]/h2)').strip()
                 regionlen = len(winehtml.xpath('//div[@id="wine_copy_inner"]/ul/li'))
                 if regionlen == 0:
-                    country,region,sub_region = '','',''
+                    country, region, sub_region = '', '', ''
                 elif regionlen == 1:
                     country = winehtml.xpath('string(//div[@id="wine_copy_inner"]/ul/li[1]/a)')
                     region, sub_region = '', ''
@@ -179,37 +184,45 @@ def crawl(id):
                     country = winehtml.xpath('string(//div[@id="wine_copy_inner"]/ul/li[1]/a)')
                     region = winehtml.xpath('string(//div[@id="wine_copy_inner"]/ul/li[2]/a)')
                     sub_region = ''
-                elif regionlen >2:
+                elif regionlen > 2:
                     country = winehtml.xpath('string(//div[@id="wine_copy_inner"]/ul/li[1]/a)')
                     region = winehtml.xpath('string(//div[@id="wine_copy_inner"]/ul/li[2]/a)')
-                    sub_region = winehtml.xpath('string(//div[@id="wine_copy_inner"]/ul/li['+str(regionlen)+']/a)')
-                # print(str(('0',id,old_rule,changUni(name),vintage,changUni(country),changUni(region),changUni(sub_region),score)))
-                mysql = "INSERT INTO `crawl_cellar` (`type`,`classNameId`,`className`,`wineName`,`vintage`,`country`,`region`,`sub_region`,`score`)" \
-                        "VALUES"+(str(('0',id,old_rule,changUni(name),vintage,changUni(country),changUni(region),changUni(sub_region),score)))
+                    sub_region = winehtml.xpath('string(//div[@id="wine_copy_inner"]/ul/li[' + str(regionlen) + ']/a)')
+                # print(str(('CT', '1',id,old_rule,changUni(name),vintage,changUni(country),changUni(region),changUni(sub_region),score)))
+                mysql = "INSERT INTO `crawl` (`from`, `type`,`classNameId`,`className`,`wineName`,`vintage`,`country`,`region`,`sub_region`,`score`)" \
+                        "VALUES" + (str(('CT', '1', id, old_rule, changUni(name), vintage, changUni(country),
+                                         changUni(region), changUni(sub_region), score)))
                 sqlLock.acquire()
                 cursor2.execute(mysql)
                 sql2.commit()
                 sqlLock.release()
     else:
-        # print(str(('0', id, old_rule)))
-        mysql = "INSERT INTO `crawl_cellar` (`type`,`classNameId`,`className`)VALUES" + (str(('0', id, old_rule)))
+        # print(str(('CT', '1', id, old_rule)))
+        mysql = "INSERT INTO `crawl` (`from`, `type`, `classNameId`, `className`) VALUES " + (
+            str(('CT', '1', id, old_rule)))
         sqlLock.acquire()
         cursor2.execute(mysql)
         sql2.commit()
         sqlLock.release()
 
+
 def main():
     concurrentpool = ThreadPoolExecutor(max_workers=20)
     # 线程数和线程名
-    # concurrentLists = []
-    # for id in getRules()[100:106]:
-    #     g = concurrentpool.submit(crawl, id)
-    #     concurrentLists.append(g)
-    # 查看线程
-    # for t in concurrentLists:
-    #     print(t.result())
-    concurrentpool.map(crawl, getRules()[4000:5000])
+    concurrentLists = []
+    for id in getRules():
+        g = concurrentpool.submit(crawl, id)
+        concurrentLists.append(g)
+    # concurrentpool.map(crawl, [33647,38489,38490,38485])
+    wait(concurrentLists, return_when=FIRST_COMPLETED)
+    for future in as_completed(concurrentLists):
+        data = future.done()
+        if data is True:
+            future.cancel()
+
 
 if __name__ == '__main__':
-    main()
-    print('finished')
+    try:
+        main()
+    finally:
+        print('\r\nFinished')
